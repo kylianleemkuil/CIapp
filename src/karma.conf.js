adb711691d8a9d4fcce9fbab3f2db7c160ad357a
KYLIAN-PC\KYLIAN <kylianleemkuil@hotmail.com>
2019-04-14 22:02:03 +0200

    karma setup phantom

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+	  require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -15,18 +16,33 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+	files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+	preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+	coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+	angularCli: {
+      environment: 'dev'
+    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage/CIapp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: config.angularCli && config.angularCli.codeCoverage
+      ? ['progress', 'coverage-istanbul']
+      : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['PhantomJS'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
